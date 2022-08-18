415c90f5446b80beedb81c7f9fd48d042bc41412
mariaflorenciaferreira <florferreirag@gmail.com>
2022-08-18 16:33:24 -0300

    css angular

--- src/app/counter/counter.component.spec.ts
+++ src/app/counter/counter.component.spec.ts
@@ -1,36 +1,66 @@
-import { TestBed } from '@angular/core/testing';
 import { CounterComponent } from './counter.component';
-describe("CounterComponent",()=>{
+import { TestBed, ComponentFixture } from '@angular/core/testing';
+import { ButtonAddComponent } from './button-add/button-add.component';
+
+describe("CounterComponent Unit", () =>{
 
     beforeEach(async () => {
         await TestBed.configureTestingModule({
-            declarations: [CounterComponent],
+          declarations: [ CounterComponent, ButtonAddComponent ],
         }).compileComponents();
     });
 
-    it("se debe crear el componente counter",()=>{
-        const fixture=TestBed.createComponent(CounterComponent)
+    it("se debe crear el componente Counter", () =>{
+        const fixure = TestBed.createComponent(CounterComponent)
 
-    expect(fixture.componentInstance).toBeTruthy()
-    
-    })
+        expect(fixure.componentInstance).toBeTruthy();
+    });
 
-    it("se debe crear el texto Contador: 23 ",()=>{
-        const fixture=TestBed.createComponent(CounterComponent)
-        
-        // para verificar si cambia algo luego de hacer click
-        fixture.detectChanges()
+    it("se debe crear el texto Counter: 23", () =>{
+        const fixure = TestBed.createComponent(CounterComponent)
+        fixure.detectChanges()
 
-        const compiled: HTMLElement=fixture.nativeElement
-       
-        expect(compiled.querySelector("h1")?.textContent).toEqual("Contador: 23 ")
-    })
+        const compiled: HTMLElement = fixure.nativeElement
 
-    it(
-        "valor inicial del contador es 23",()=>{
-            const counter= new CounterComponent()
 
-            expect(counter.contador).toBe(23)
-        }
-    )
-})
+        expect(compiled.querySelector("h1")?.textContent).toEqual("Contador: 23");
+    });
+
+    it("valor inicial del contador es 23", () =>{
+
+        const counter = new CounterComponent()
+
+        expect(counter.contador).toBe(23);
+    });
+});
+
+describe("Integration Testing", () => {
+
+    let component: CounterComponent;
+    let fixture: ComponentFixture<CounterComponent>;
+
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [ CounterComponent, ButtonAddComponent ],
+        }).compileComponents();
+    });
+
+    beforeEach(() =>{
+        fixture = TestBed.createComponent(CounterComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+    it("evento click", () =>{
+        const compiled: HTMLElement = fixture.nativeElement;
+        const counterValue= compiled.querySelector("h1")!;
+
+        const btnAdd: HTMLElement = fixture.debugElement.nativeElement.querySelector("#add");
+
+        btnAdd.click();
+        fixture.detectChanges();
+        expect(counterValue.textContent).toEqual("Contador: 24");
+
+
+    });
+});
--- src/app/counter/counter.component.ts
+++ src/app/counter/counter.component.ts
@@ -1,8 +1,10 @@
 import { Component } from "@angular/core";
 
+
 @Component({
     selector:"app-contador",
     templateUrl:"./counter.component.html",
+    styleUrls:["./counter.component.css"]
 })
 export class CounterComponent{
     // puedo crear variables
